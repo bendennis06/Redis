ae287ec0ffa235cb04678b293226424ff9fe14c7
bendennis06 <130309802+bendennis06@users.noreply.github.com>
2025-07-02 18:31:14 -0700

    [any message]

--- app/main.go
+++ app/main.go
@@ -21,9 +21,14 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+
+		conn, err := l.Accept() //go's short variable declaration := declares and assigns at the same time
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		conn.Write([]byte("+PONG\r\n"))
 	}
 }
