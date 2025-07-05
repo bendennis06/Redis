30ac06778c858c37a2bce875029b76fa6de124f3
bendennis06 <130309802+bendennis06@users.noreply.github.com>
2025-07-05 15:34:07 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,9 +2,65 @@ package main
 
 import (
 	"fmt"
+	_ "go/types"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
+
+// Take the raw bytes (or string) your server reads from the connection.
+// Parse the Redis RESP protocol.
+// Return a slice []string → the command name and its arguments.
+// //ex *2\r\n$5\r\nhello\r\n$5\r\nworld\r\n
+func parseRESP(b []byte) ([]string, error) {
+
+	if len(b) == 0 {
+		return nil, fmt.Errorf("input to short") //no data to read
+	}
+
+	if b[0] != '*' {
+		return nil, fmt.Errorf("expected array at top level") //needs to start with '*'
+	}
+
+	index := 1
+	//look for '\r\n'
+	for index < len(b) && !(b[index] == '\r' && b[index+1] == '\n') {
+		index++
+	}
+
+	arraySizeString := string(b[1:index])            //string slice, grab array length
+	arrSizeInt, err := strconv.Atoi(arraySizeString) //convert string to int
+	if err != nil {
+		return nil, fmt.Errorf("not valid array size", err)
+	}
+
+	index += 2 //skip past '\r\n
+
+	parts := []string{}               //[] slice(dynamic array) that holds strings, {} ini as empty
+	for j := 0; j < arrSizeInt; j++ { //runs for each num of strings (*n) will run n times
+		if b[index] != '$' {
+			return nil, fmt.Errorf("expected bulk string", b[index]) //look for '$' (bulk strings)
+		}
+		index++
+
+		startInt := index //start of string
+		for index < len(b) && !(b[index] == '\r' && b[index+1] == '\n') {
+			index++
+		}
+		bulkLenStr := string(b[startInt:index])  //length of string (num after $)
+		bulkLen, err := strconv.Atoi(bulkLenStr) //convert length to int
+		if err != nil {
+			return nil, fmt.Errorf("bad bulk length", err)
+		}
+		index += 2                               //skip past '\r\n'
+		part := string(b[index : index+bulkLen]) //extract bulk string
+		parts = append(parts, part)
+
+		index += bulkLen + 2 //move past string and last \r\n
+	}
+	return parts, nil
+}
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
@@ -15,10 +71,26 @@ func handleConnection(conn net.Conn) {
 		if err != nil {
 			return
 		}
-		input := string(buffer[:n])
-		if input == "*1\r\n$4\r\nPING\r\n" {
+
+		//parse resp
+		commands, err := parseRESP(buffer[:n])
+		if err != nil {
+			fmt.Println("error parsing", err)
+			continue
+		}
+
+		//check command is ping
+		if len(commands) == 1 && strings.ToUpper(commands[0]) == "PING" {
 			conn.Write([]byte("+PONG\r\n"))
 		}
+		//check if command is echo
+		if len(commands) == 2 && strings.ToUpper(commands[0]) == "ECHO" {
+			response := fmt.Sprintf("$%d\r\n%s\r\n", len(commands[1]), commands[1]) //build a formatted string
+			conn.Write([]byte(response))                                            // return formatted string
+		}
+		//if input == "*1\r\n$4\r\nPING\r\n" {
+		//	conn.Write([]byte("+PONG\r\n"))
+		//}
 	}
 }
 
@@ -46,6 +118,6 @@ func main() {
 			continue
 			//os.Exit(1)
 		}
-		go handleConnection(conn)
+		go handleConnection(conn) //handles each connection in its own goroutine
 	}
 }
